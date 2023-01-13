508c9702e090b37f80a4957ceaa4ae422d8ce82e
wwwalohacampus <h850415@naver.com>
2023-01-13 17:53:52 +0900

    컨텐트 타입 WEBP 추가, "정말로 삭제하시겠습니까?" 추가

--- spring-board-file/src/main/java/com/human/utils/MediaUtil.java
+++ spring-board-file/src/main/java/com/human/utils/MediaUtil.java
@@ -20,6 +20,7 @@ public class MediaUtil {
 		mediaType.put("JPEG", MediaType.IMAGE_JPEG);
 		mediaType.put("GIF", MediaType.IMAGE_GIF);
 		mediaType.put("PNG", MediaType.IMAGE_PNG);
+		mediaType.put("WEBP",  MediaType.parseMediaType("img/webp") );		// MediaType.IMAGE_WEBP 상수는 존재하지 않음 
 	}
 	
 	// 확장자로 컨텐트타입을 확인하는 메소드
