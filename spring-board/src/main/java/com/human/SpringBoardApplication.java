d12e8160570c296f2d284f337251e5390dca98ce
wwwalohacampus <h850415@naver.com>
2023-01-13 16:09:03 +0900

    spring-board

---
+++ spring-board/src/main/java/com/human/SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardApplication.class, args);
+	}
+
+}
---
+++ spring-board/src/main/java/com/human/domain/Board.java
@@ -0,0 +1,23 @@
+package com.human.domain;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+
+// @Alias("Board")   // 생략하면, 클래스명으로 별칭을 설정한다
+@Data
+public class Board {
+	
+	private int boardNo;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private Date updDate;
+	
+
+}
